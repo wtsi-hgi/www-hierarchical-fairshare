51b46e3fb5d86d4e433af1d6506233d76d6c59c7
Joshua Randall <joshua.randall@sanger.ac.uk>
2012-11-30 13:52:42 +0000

    Added license text.

--- lsb-users-graph.pl
+++ lsb-users-graph.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/env perl 
 
+# Copyright (c) 2012 Genome Research Ltd. 
+# Author: Joshua C. Randall <joshua.randall@sanger.ac.uk>
+#
+# This file is part of www-hierarchical-fairshare.
+#
+# www-hierarchical-fairshare is free software: you can redistribute it
+# and/or modify it under the terms of the GNU Affero General Public
+# License as published by the Free Software Foundation; either version
+# 3 of the License, or (at your option) any later version.  This
+# program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Affero General Public License for more details.  You should have
+# received a copy of the GNU Affero General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+
 use strict;
 use warnings;
 
