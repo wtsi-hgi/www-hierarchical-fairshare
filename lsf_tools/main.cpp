fa020b68163528236abd92ca6b6f851e61c4ba5f
Nick Clarke <nc6@sanger.ac.uk>
2013-01-16 14:08:28 +0000

    Finish work on the normalising - now getting the right size of boxes and everything.
    Also give it a nicer green colour.

--- lsf_tools/main.cpp
+++ lsf_tools/main.cpp
@@ -8,118 +8,148 @@
 
 class ShareAcct
 {
-public:
-    void print()
-    {
-        printf( "{Group:'%s',Parent:'%s',Percentage:%d,Priority:%f}\n", group.c_str(), parent.c_str(), shares, priority);
+  public:
+    static void print_structure() {
+      printf("['Group', 'Parent', 'Percentage', 'Priority']");
     }
-    void print_normalised(float by) {
-        printf( "{Group:'%s',Parent:'%s',Percentage:%d,Priority:%f}\n", group.c_str(), parent.c_str(), shares, priority / by);
+    void print(bool is_parent) {
+      if (is_parent) {
+        printf( "['%s','%s',%f,%f]\n", group.c_str(), parent.c_str(), proportion, priority);
+      } else {
+        printf( "['%s@%s','%s',%f,%f]\n", group.c_str(), parent.c_str(), parent.c_str(), proportion, priority);
+      }
     }
-private:
+  private:
     std::string group;
     std::string parent;
     int shares;
+    float proportion;
     float priority;
 
     static std::pair<std::string,std::string>* digestPath( const char *input )
     {
-        char* manip = strdup(input);
-        ssize_t orig_size = strlen(manip);
-        int tokens_found = 0;
-        for (ssize_t loop_iter = 0; loop_iter < strlen(manip); loop_iter++)
-        {
-            if (manip[loop_iter] == '/') {
-                tokens_found++;
-            }
+      char* manip = strdup(input);
+      ssize_t orig_size = strlen(manip);
+      int tokens_found = 0;
+      for (ssize_t loop_iter = 0; loop_iter < strlen(manip); loop_iter++)
+      {
+        if (manip[loop_iter] == '/') {
+          tokens_found++;
         }
-        char** tokens = (char**) malloc(sizeof(char*)*(tokens_found+1));
-        tokens[0] = manip;
-        int token_pos = 1;
-        for (ssize_t loop_iter = 0; loop_iter < orig_size; loop_iter++)
-        {
-            if (manip[loop_iter] == '/') {
-                manip[loop_iter] = '\0';
-                tokens[token_pos] = manip+loop_iter+1;
-                token_pos++;
-            }
+      }
+      char** tokens = (char**) malloc(sizeof(char*)*(tokens_found+1));
+      tokens[0] = manip;
+      int token_pos = 1;
+      for (ssize_t loop_iter = 0; loop_iter < orig_size; loop_iter++)
+      {
+        if (manip[loop_iter] == '/') {
+          manip[loop_iter] = '\0';
+          tokens[token_pos] = manip+loop_iter+1;
+          token_pos++;
         }
-        return new std::pair<std::string,std::string>(tokens[tokens_found], tokens[tokens_found-1]);
+      }
+      return new std::pair<std::string,std::string>(tokens[tokens_found], tokens[tokens_found-1]);
     }
-public:
+  public:
     ShareAcct(const struct shareAcctInfoEnt *curr_share)
     {
-        std::pair<std::string, std::string>* path = digestPath(curr_share->shareAcctPath);
-        group = path->first;
-        parent = path->second;
-        delete path;
+      std::pair<std::string, std::string>* path = digestPath(curr_share->shareAcctPath);
+      group = path->first;
+      parent = path->second;
+      delete path;
 
-        shares = curr_share->shares;
-        priority = curr_share->priority;
+      shares = curr_share->shares;
+      priority = curr_share->priority;
+      proportion = 1.123;
     }
 
     const char* getParent() { return parent.c_str(); }
-
+    const char* getGroup() { return group.c_str(); }
     const float getPriority() { return priority; }
+    const float getShares() { return shares; }
+    const float getProportion() { return proportion; }
+
+    void setProportion(const float p) { proportion = p; }
 };
 
 int main(int argc, char** argv)
 {
-    // Initialise our connection to LSF
-    if (lsb_init(argv[0]) < 0) {
-        lsb_perror("lsb_init() failed");
-        return -1;
+  // Initialise our connection to LSF
+  if (lsb_init(argv[0]) < 0) {
+    lsb_perror("lsb_init() failed");
+    return -1;
+  }
+
+  // If queue argument isn't defined then just show data from normal job queue
+  char* queues;
+  if (argc < 2) {
+    queues = strdup("normal");
+  }
+  else {
+    queues = argv[1];
+  }
+
+  // Query LSF
+  int num_queues = 1;
+  struct queueInfoEnt *info = lsb_queueinfo(&queues, &num_queues, NULL, NULL, 0);
+  // For each queue in output (should only be one) process data
+  for (int i = 0; i < num_queues; i++) {
+    struct queueInfoEnt *curr_info = &info[i];
+    std::map<std::string, float> parent_map;
+    std::list<ShareAcct*> accounts;
+    // SKUNKWORKS SOLUTION
+    for (int j = 1; j < curr_info->numOfSAccts; j++) {
+      struct shareAcctInfoEnt *curr_share = &curr_info->shareAccts[j];
+      ShareAcct* acct = new ShareAcct( curr_share );
+      std::string parent = std::string(acct->getParent());
+      // If the parent is already in the map, sum the priorities. Otherwise, add it in with the priority of the first child.
+      if (parent_map.count(parent) > 0) {
+        parent_map[parent] += acct->getShares();
+      } else {
+        parent_map[parent] = acct->getShares();
+      }
+
+      accounts.push_back(acct);
     }
 
-    // If queue argument isn't defined then just show data from normal job queue
-    char* queues;
-    if (argc < 2) {
-        queues = strdup("normal");
-    }
-    else {
-        queues = argv[1];
-    }
-
-    // Query LSF
-    int num_queues = 1;
-    struct queueInfoEnt *info = lsb_queueinfo(&queues, &num_queues, NULL, NULL, 0);
-    // For each queue in output (should only be one) process data
-    for (int i = 0; i < num_queues; i++) {
-        struct queueInfoEnt *curr_info = &info[i];
-        std::map<std::string,float> parent_map;
-        std::list<ShareAcct*> accounts;
-        // SKUNKWORKS SOLUTION
-        for (int j = 1; j < curr_info->numOfSAccts; j++) {
-            struct shareAcctInfoEnt *curr_share = &curr_info->shareAccts[j];
-            ShareAcct* acct = new ShareAcct( curr_share );
-            std::string parent = std::string(acct->getParent());
-            // If the parent is already in the map, sum the priorities. Otherwise, add it in with the priority of the first child.
-            if (parent_map.count(parent) > 0) {
-                parent_map[parent] += acct->getPriority();
-            } else {
-                parent_map[parent] = acct->getPriority();
-            }
-
-            accounts.push_back(acct);
+    // Now walk back through the accounts (reverse order) and first normalise shares,
+    // then multiply that by the value we calculated for the parents. We do this recursively until
+    // we hit the bottom.
+    std::map<std::string, float> share_map;
+    for (auto it = accounts.begin(); it != accounts.end(); it++) {
+      ShareAcct* a = *it;
+      if (parent_map.count(a->getParent()) > 0) {
+        if (share_map.count(a->getParent()) > 0) {
+          share_map[a->getGroup()] = ((a->getShares() / parent_map[a->getParent()]) * share_map[a->getParent()]);
+        } else {
+          share_map[a->getGroup()] = (a->getShares() / parent_map[a->getParent()]);
         }
-        // /SKUNKWORKS SOLUTION
+      } else {
+        share_map[a->getGroup()] = 1;
+      }
 
-        // If there are actually any share accounts defined for this queue write result
-        // It's done this way because JSON doesn't support commas at the end of the last list item
-        if (curr_info->numOfSAccts > 0) {
-            printf("[\n");
-            auto it = accounts.begin();
-            // Write first element
-            (*it)->print_normalised(parent_map[(*it)->getParent()]);
-            // Write subsequent elements
-            for (; it != accounts.end(); it++) {
-                printf(",\n");
-                (*it)->print_normalised(parent_map[(*it)->getParent()]);
-            }
-            printf("]\n");
-            }
-        }
-        return 0;
+      a->setProportion(share_map[a->getGroup()]);
+
     }
 
 
+    // /SKUNKWORKS SOLUTION
+    std::string root = accounts.front()->getParent();
+
+    // If there are actually any share accounts defined for this queue write result
+    // It's done this way because JSON doesn't support commas at the end of the last list item
+    if (curr_info->numOfSAccts > 0) {
+      printf("[\n");
+      ShareAcct::print_structure();
+      // Write the root node
+      printf("\n,\n['%s','' , 1, 1]", root.c_str());
+      // Write elements
+      for (auto it = accounts.begin(); it != accounts.end(); it++) {
+        printf(",\n");
+        (*it)->print(parent_map.count((*it)->getGroup()) > 0);
+      }
+      printf("]\n");
+    }
+  }
+  return 0;
+}
--- web/lsb-users-graph.pl
+++ web/lsb-users-graph.pl
@@ -19,181 +19,8 @@
 use strict;
 use warnings;
 
-use Data::Dumper;
-use Tree::DAG_Node;
-
-my $lsb_users_file = shift || "/usr/local/lsf/conf/lsbatch/farm2/configdir/lsb.users";
-
-my %nodes;
-my $tree_root = Tree::DAG_Node->new();
-$tree_root->name("/");
-$nodes{"/"} = $tree_root;
-
-my $line = ""; 
-
-my %usergroups; 
-my %nonroot_members;    # keep track of all group members (non-root nodes)
-
-my $lsb_users_fh;
-open $lsb_users_fh, "<$lsb_users_file" or die "could not open file $lsb_users_file\n";
-
-my $ug = 0; 
-while($line = <$lsb_users_fh>) {
-    if($line =~ m/^Begin\ UserGroup/) {
-	$ug=1;
-    } elsif($line =~ m/^End\ UserGroup/) {
-	$ug=0;
-    } else {
-	if($ug==1) {
-	    if(!( $line =~ m/(^#|^$|^GROUP_NAME)/)) {
-		$line =~ m/^(.*?)[[:space:]]*\((.*?)\)[[:space:]]*\((.*)\)[[:space:]]*$/ or die "could not parse line $line\n"; 
-		my $group=$1; 
-		my $members=$2; 
-		my $sharetuples=$3; 
-
-		$members =~ s/^[[:space:]]+//;
-		$members =~ s/[[:space:]]+$//;
-		my @members = split /[[:space:]]+/,$members; 
-
-		$sharetuples =~ s/^[[:space:]]*\[[[:space:]]*//; 
-		$sharetuples =~ s/[[:space:]]*\][[:space:]]*$//; 
-		my @sharetuples = split /[[:space:]]*\][[:space:]]*\[[[:space:]]*/, $sharetuples; 
-		my %share; 
-		foreach my $tuple (@sharetuples) {
-		    my ($member, $share) = split /[[:space:]]*\,[[:space:]]*/, $tuple;
-		    $share{$member} = $share;
-		}
-		
-		foreach my $member (@members) {
-		    $nonroot_members{$member} = 1;
-		    if(exists($share{$member})) {
-			$usergroups{$group}{$member} = $share{$member};
-		    } else {
-			exists($share{default}) or die "no explicit share for [$member] in group [$group] and no default set\n";
-			$usergroups{$group}{$member} = $share{default};
-		    }
-		}
-	    }
-	}
-    }
-}
-close $lsb_users_fh;
-
-
-foreach my $usergroup (keys %usergroups) {
-    # create tree node for usergroup
-    if(!exists($nodes{$usergroup})) {
-	my $node = $tree_root->new();
-	$node->name($usergroup);
-	$nodes{$usergroup} = $node;
-    }
-    
-    if(!exists($nonroot_members{$usergroup})) {
-	# this is a root node, connect it
-	$tree_root->add_daughter($nodes{$usergroup});
-	$nodes{$usergroup}->attributes->{'share'} = 1;
-    }
-
-    
-    foreach my $member (keys %{$usergroups{$usergroup}}) {
-	my $node_name = $member;
-	if(!exists($usergroups{$member})) {
-	    # this is a user, name it per group
-	    $node_name = "$usergroup:$member";
-	}
-	
-	if(!exists($nodes{$node_name})) {
-	    my $node = $tree_root->new();
-	    $node->name($node_name);
-	    $nodes{$node_name} = $node;
-	} 
-	$nodes{$usergroup}->add_daughter($nodes{$node_name});
-	
-	$nodes{$node_name}->attributes->{'share'} = $usergroups{$usergroup}{$member};
-
-	#print STDERR "added node $node_name to parent $usergroup with share ".$usergroups{$usergroup}{$member}."\n";
-    }
-}
-
-#print Dumper($tree_root)."\n";
-#print map "$_\n", @{$tree_root->draw_ascii_tree};
-
-my @stack;
-$tree_root->walk_down({
-    callback => sub {
-	my ($node, $options) = @_;
-	
-	my $share = $node->attributes->{'share'};
-	my $name = $node->name;
-
-	my $total_share_at_depth = 0;
-	foreach my $sister_node ( $node->self_and_sisters() ) {
-	    if(exists($sister_node->attributes->{'share'})) {
-		$total_share_at_depth += $sister_node->attributes->{'share'};
-	    }
-	}
-
-	my $share_ratio_at_depth = 1;
-	if($total_share_at_depth > 0) {
-	    $share_ratio_at_depth = $share / $total_share_at_depth;
-	}
-
-	push @{$$options{stack}}, {
-	    name => $name,
-	    share_ratio_at_depth => $share_ratio_at_depth,
-	    total_share_at_depth => $total_share_at_depth,
-	};
-	return 1;
-    },
-    _depth => 0,
-    stack => \@stack,
-		      });
-
-foreach my $share_change (@stack) {
-#    print Dumper($share_change)."\n";
-    my $name = $share_change->{'name'};
-    if(exists($nodes{$name})) {
-	$nodes{$name}->attributes->{'share_ratio_at_depth'} = $share_change->{'share_ratio_at_depth'};
-	$nodes{$name}->attributes->{'total_share_at_depth'} = $share_change->{'total_share_at_depth'};
-    } else {
-	die "nodes{$name} does not exist";
-    }
-}
-
-
-@stack = ();
-$tree_root->walk_down({
-    callback => sub {
-	my ($node, $options) = @_;
-	
-	my $overall_share = $node->attributes->{'share_ratio_at_depth'};
-	foreach my $desc_node ($node->ancestors()) {
-	    my $share_ratio_at_depth = $desc_node->attributes->{'share_ratio_at_depth'};
-	    $overall_share *= $share_ratio_at_depth;
-	}
-
-	my $name = $node->name;
-
-	push @{$$options{stack}}, {
-	    name => $name,
-	    overall_share => $overall_share,
-	};
-	return 1;
-    },
-    _depth => 0,
-    stack => \@stack,
-		      });
-foreach my $overall_share_change (@stack) {
-#    print Dumper($share)."\n";
-    my $name = $overall_share_change->{'name'};
-    if(exists($nodes{$name})) {
-	$nodes{$name}->attributes->{'overall_share'} = $overall_share_change->{'overall_share'};
-    } else {
-	die "nodes{$name} does not exist";
-    }
-}
-
-
+use File::Spec::Functions qw(rel2abs);
+use File::Basename;
 
 print <<"EOF";
 <html>
@@ -207,32 +34,10 @@ function drawChart() {
         var data = google.visualization.arrayToDataTable(
 EOF
 
-@stack = ();
-my @rows;
-push @rows, "['Group','Parent','Percentage']";
-$tree_root->walk_down({
-    callback => sub {
-	my ($node, $options) = @_;
-	
-	my $name = $node->name;
-	my $overall_share = $node->attributes->{'overall_share'};
-	
-	if($options->{_depth} == 0) {
-	    # root node
-	    push @rows, "['".$name."', null, $overall_share]";
-	} else {
-	    my $parent = $node->mother->name;
-	    if(! ($name =~ m/\:/)) {
-		push @rows, "['".$name."', '".$parent."', ".$overall_share."]";
-	    }
-	}
-	
-	return 1;
-    },
-    _depth => 0,
-    stack => \@stack,
-		      });
-print "[".join(",\n", @rows)."]\n";
+my $path = dirname(rel2abs($0));
+my $data = `$path/../lsf_tools/lsf_fairshare`;
+$data =~ s/(?>\x0D\x0A?|[\x0A-\x0C\x85\x{2028}\x{2029}])//g;
+print $data;
 
 
 print <<"EOF";
@@ -241,9 +46,9 @@ print <<"EOF";
         // Create and draw the visualization.
 	    var tree = new google.visualization.TreeMap(document.getElementById('chart_div'));
         tree.draw(data, {
-          minColor: '#ddd',
-          midColor: '#ddd',
-          maxColor: '#ddd',
+          minColor: '#030',
+          midColor: '#090',
+          maxColor: '#0f0',
           noColor: '#ddd',
 	  headerColor: '#aaa',
           minHighlightColor: '#0ff',
@@ -266,3 +71,4 @@ print <<"EOF";
   </body>
 </html>
 EOF
+
